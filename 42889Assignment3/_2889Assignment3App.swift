313a5e9ef23eabf1becb63930fa026005907288c
yanliang.huang@student.uts.edu.au <yanliang.huang@student.uts.edu.au>
2024-04-23 14:00:47 +1000

    first commit

---
+++ 42889Assignment3/_2889Assignment3App.swift
@@ -0,0 +1,17 @@
+//
+//  _2889Assignment3App.swift
+//  42889Assignment3
+//
+//  Created by Graham Huang on 23/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct _2889Assignment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
