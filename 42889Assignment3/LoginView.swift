7b54a70bbaddf8a398e75a91e60adc5568aacd48
yanliang.huang@student.uts.edu.au <yanliang.huang@student.uts.edu.au>
2024-05-07 17:54:02 +1000

    add comments to login view

--- 42889Assignment3/LoginView.swift
+++ 42889Assignment3/LoginView.swift
@@ -3,12 +3,12 @@ import SwiftUI
 struct LoginView: View {
     @State private var username: String = ""
     @State private var password: String = ""
-    @State private var isLoggedIn = false // 控制导航状态
+    @State private var isLoggedIn = false // control login status
     @StateObject var authModel = AuthenticationModel()
     @StateObject var dataProvider = DataProvider()
     
     var body: some View {
-        NavigationStack {  // 使用 NavigationStack 替代 NavigationView
+        NavigationStack {
             VStack {
                 TextField("Username", text: $username)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
@@ -21,7 +21,7 @@ struct LoginView: View {
                 Button("Login") {
                     if authModel.authenticate(username: username, password: password) {
                         print("Login successful")
-                        isLoggedIn = true // 设置为 true，触发导航
+                        isLoggedIn = true 
                     } else {
                         print("Login failed")
                     }
@@ -30,8 +30,6 @@ struct LoginView: View {
                 
                 NavigationLink("Register", destination: RegistrationView())
                     .padding()
-                
-                // 这个 navigationDestination 必须在 NavigationStack 中使用
                 .navigationDestination(isPresented: $isLoggedIn) {
                     SelectCinemaView().environmentObject(dataProvider)
                 }
