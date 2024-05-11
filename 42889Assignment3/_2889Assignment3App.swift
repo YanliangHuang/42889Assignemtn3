fc48b775c98ecad97f614a47971ca692f140dfc7
Zeyu Cui <zeyu.cui@student.uts.edu.au>
2024-05-11 20:38:05 +1000

    add supabase package

--- 42889Assignment3/_2889Assignment3App.swift
+++ 42889Assignment3/_2889Assignment3App.swift
@@ -1,27 +1,27 @@
-//
-//  _2889Assignment3App.swift
-//  42889Assignment3
-//
-//  Created by Graham Huang on 23/4/2024.
-//
-
+import Supabase
 import SwiftUI
 
 @main
 struct _2889Assignment3App: App {
     @StateObject var dataProvider = DataProvider()
     @StateObject var authModel: AuthenticationModel
-    
+
+    let client: SupabaseClient
+
     init() {
-        //init everything. dataprovider should be database related in future update
-        //authmodel is responsible for login, logout and register
         let dataProvider = DataProvider()
         let authModel = AuthenticationModel(dataProvider: dataProvider)
 
         _dataProvider = StateObject(wrappedValue: dataProvider)
         _authModel = StateObject(wrappedValue: authModel)
+
+        // initialize supabase client
+        self.client = SupabaseClient(
+            supabaseURL: URL(string: "https://yzonusubkjiizrlpxnxv.supabase.co")!,
+            supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6b251c3Via2ppaXpybHB4bnh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ4OTgzNzEsImV4cCI6MjAzMDQ3NDM3MX0.2yqgOtQRRTylyiq2ROOs46Gf5f2kzUGh6YBsdCK_G1Y"
+        )
     }
-    
+
     var body: some Scene {
         WindowGroup {
             LoginView().environmentObject(authModel).environmentObject(dataProvider)
